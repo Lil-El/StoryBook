4632a4524feb142debf742851f7a1fa898280150
Lil-El <yxd99324@qq.com>
2020-07-22 21:58:09 +0800

    use addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,19 @@
+const path = require("path")
+// story 全局配置
 module.exports = {
-  stories: ["../src/**/*.stories.[tj]s"],
+  stories: ["../src/stories/**/*.stories.js"],
+  addons: [
+    {
+      name: "@storybook/addon-storysource",
+      options: {
+        rule: {
+          // test: [/\.stories\.jsx?$/], This is default
+          include: [path.resolve(__dirname, "../src/stories")], // 找到自己存放stories的文件夹
+        },
+        loaderOptions: {
+          prettierConfig: { printWidth: 80, singleQuote: false },
+        },
+      },
+    },
+  ],
 };
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # StoryBook 踩坑
+- 在.storybook配置解析路径
+- 编写story
+npm i @storybook/addon-storysource --save-dev 查看story的源码
